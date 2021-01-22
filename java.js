f23db6ed1c5857e2e3beae3359fe7a50b8ea0352
dlmalhao <dlmalhao@gmail.com>
2021-01-22 22:46:19 +0000

    Log-in / Register Completed 2

--- java.js
+++ java.js
@@ -1,22 +1,30 @@
 
-function confirmation() {
+// function confirmation() {
 
-    let pass = document.getElementById("pass").value
-    let confirm_pass = document.getElementById("confirm-pass").value
+//     let pass = document.getElementById("pass").value
+//     let confirm_pass = document.getElementById("confirm-pass").value
 
-    if (pass == confirm_pass) {
-        alert("Registered with success!")
+//     if (pass == confirm_pass) {
+//         alert("Registered with success!")
 
-        setTimeout(function(){ window.location.href = "index.html"; }, 2000);
-    }
-    else {
-        alert("Password and Confirm Password are different. \n Please try again.")
-    }
-    return false
-}
+//         setTimeout(function(){ window.location.href = "index.html"; }, 2000);
+//     }
+//     else {
+//         alert("Password and Confirm Password are different. \n Please try again.")
+//     }
+//     return false
+// }
 
 function enter_site() {
-    alert("Logged in with success!")
-    setTimeout(function(){ window.location.href = "index.html"; }, 2000);
+    let x = document.getElementById("pass").value
+    let y = document.getElementById("username1").value
+
+    if (x == "" || y == "") {
+        alert("ERROR!\nPlease introduce your credentials.")
+    }
+    else {
+        alert("Logged in with success!")
+        setTimeout(function(){ window.location.href = "index.html"; }, 2000);
+    }
     return false
 }
