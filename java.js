3713abbabd85c7c63f86eaedb483654b5a5cb3c9
dlmalhao <dlmalhao@gmail.com>
2021-01-25 02:39:33 +0000

    Site com store e index em falta

--- java.js
+++ java.js
@@ -1,19 +1,19 @@
 
-// function confirmation() {
+ function confirmation() {
 
-//     let pass = document.getElementById("pass").value
-//     let confirm_pass = document.getElementById("confirm-pass").value
+     let pass = document.getElementById("pass").value
+     let confirm_pass = document.getElementById("confirm-pass").value
 
-//     if (pass == confirm_pass) {
-//         alert("Registered with success!")
+     if (pass == confirm_pass) {
+         alert("Registered with success!")
 
-//         setTimeout(function(){ window.location.href = "index.html"; }, 2000);
-//     }
-//     else {
-//         alert("Password and Confirm Password are different. \n Please try again.")
-//     }
-//     return false
-// }
+         setTimeout(function(){ window.location.href = "index.html"; }, 2000);
+     }
+     else {
+         alert("Password and Confirm Password are different. \n Please try again.")
+     }
+     return false
+ }
 
 function enter_site() {
     let x = document.getElementById("pass").value
@@ -24,6 +24,23 @@ function enter_site() {
     }
     else {
         alert("Logged in with success!")
+        setTimeout(function(){ window.location.href = "index.html"; }, 2000);
+    }
+    return false
+}
+
+function subscribe() {
+    let a = document.getElementById("first_name").value
+    let b = document.getElementById("last_name").value
+    let c = document.getElementById("mail").value
+    let d = document.getElementById("birth").value
+    let e = document.getElementById("locality").value
+
+    if (a == "" || b == "" || c == "" || d == "" || e == "") {
+        alert("ERROR!\nPlease introduce your credentials.")
+    }
+    else {
+        alert("Subscription completed!")
         setTimeout(function(){ window.location.href = "index.html"; }, 2000);
     }
     return false
